edfb2ac5ea034563c8f24d31a1d47a13cb21f36e
polarcube1 <49788382+polarcube1@users.noreply.github.com>
2021-01-28 14:43:50 -0500

    Add files via upload

---
+++ Marker_Based_AR/Detector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+    public bool found;
+    public GameObject present;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnCubeCO());
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        found = true;
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+    IEnumerator SpawnCubeCO()
+    {
+        yield return new WaitForSeconds(1f);
+        if (!found)
+        {
+            Instantiate(present, transform.position, transform.rotation);
+        }
+    }
+}
